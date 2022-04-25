724069ca17d47ebfc2f7c633280275edfee7f94e
“Krystian” <scoverstudio@gmail.com>
2022-04-25 16:06:45 +0200

    add verification on email and title

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -43,6 +43,24 @@ router.post("/posts", async (req, res) => {
       status,
     } = req.body;
 
+    const titlePattern = new RegExp(
+      /(<\s*(strong|em)*>(([A-z]|\s)*)<\s*\/\s*(strong|em)>)|(([A-z]|\s|\.)*)/,
+      "g"
+    );
+
+    const emailPattern = new RegExp(
+      /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+    );
+
+    const titleMatched = title.match(titlePattern).join("");
+    const emailMatched = authorEmail.match(emailPattern).join("");
+
+    if (
+      titleMatched.length < title.length ||
+      emailMatched.length < authorEmail.length
+    )
+      throw new Error("Invaild characters");
+
     if ((title && content && authorEmail, status)) {
       const newPost = new Post({
         title,
